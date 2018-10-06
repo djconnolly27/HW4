288116fc981b9123a5ba46fc5b0a57eac900ca01
djconnolly27 <djconnolly27@gmail.com>
2018-10-06 18:57:36 -0400

    Adds completed deliverables 2 and 3

--- register.v
+++ register.v
@@ -15,3 +15,38 @@ input		clk
     end
 
 endmodule
+
+module register32
+(
+  output wire [31:0] q,
+  input [31:0] d,
+  input wrenable,
+  input clk
+);
+
+  genvar bitNum;
+  generate
+    for (bitNum = 0; bitNum < 32; bitNum= bitNum + 1) begin: genRegs
+      register regOneBit(.q(q[bitNum]), .d(d[bitNum]), .wrenable(wrenable), .clk(clk));
+    end
+  endgenerate
+
+
+endmodule
+
+module register32zero
+(
+  output wire [31:0] q,
+  input [31:0] d,
+  input wrenable,
+  input clk
+);
+
+  genvar bitNum;
+  generate
+    for (bitNum = 0; bitNum < 32; bitNum= bitNum + 1) begin: genRegs
+      register regOneBit(.q(q[bitNum]), .d(1'b0), .wrenable(wrenable), .clk(clk));
+    end
+  endgenerate
+
+endmodule
