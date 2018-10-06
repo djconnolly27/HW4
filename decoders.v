2fff00c5cfd55745abd2931d20931cf68e117be0
djconnolly27 <djconnolly27@gmail.com>
2018-10-06 19:27:06 -0400

    Adds functional muxes and decoder with explanation

--- decoders.v
+++ decoders.v
@@ -8,7 +8,16 @@ input		enable,
 input[4:0]	address
 );
 
-    assign out = enable<<address; 
+    assign out = enable<<address;
 
 endmodule
 
+/*
+This decoder utilizes a binary shift operator. It shifts the value of enable
+to the left by "address" number of bits. If enable is 0, this will simply
+shift 0 to the left by "address" number of places and then pad the most
+significant bits with 0s, resulting in all output bits being sent to 0. If
+enable is 1, then this shifts that 1 to the left by "address" number of places
+and pads the most significant bits with 0 if necessary, which makes out[address]
+equal to 1, while leaving all other bits at 0.
+*/
